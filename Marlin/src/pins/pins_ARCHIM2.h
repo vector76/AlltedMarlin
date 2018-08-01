a0dd7c8d39da61f6ce8d3abcf72fef87d31b9c90
Ryan Zellars <allted@gmail.com>
2018-08-01 12:12:09 -0700

    No pin comments allowed for compile.

--- Marlin/src/pins/pins_ARCHIM2.h
+++ Marlin/src/pins/pins_ARCHIM2.h
@@ -57,10 +57,10 @@
 // Otherwise use a physical endstop based configuration.
 
 #if DISABLED(SENSORLESS_HOMING)
- #define X_MIN_PIN          14   // PD4 MIN ES1
- #define X_MAX_PIN          32   // PD10 MAX ES1
- #define Y_MIN_PIN          29   // PD6 MIN ES2
- #define Y_MAX_PIN          15   // PD5 MAX ES2
+ #define X_MIN_PIN          14
+ #define X_MAX_PIN          32
+ #define Y_MIN_PIN          29
+ #define Y_MAX_PIN          15
 #else
   #if X_HOME_DIR == -1
     #define X_MIN_PIN      X_DIAG_PIN
@@ -79,8 +79,8 @@
   #endif
 #endif
 
- #define Z_MIN_PIN          31   // PA7 MIN ES3
- #define Z_MAX_PIN          30   // PD9 MAX ES3
+ #define Z_MIN_PIN          31
+ #define Z_MAX_PIN          30
 
 //
 // Z Probe (when not Z_MIN_PIN)
@@ -92,39 +92,39 @@
 //
 // Steppers
 //
-#define X_STEP_PIN         38   // PC6 X-STEP *
-#define X_DIR_PIN          37   // PC5 X-DIR *
-#define X_ENABLE_PIN       41   // PC9 EN1
+#define X_STEP_PIN         38
+#define X_DIR_PIN          37
+#define X_ENABLE_PIN       41
 #ifndef X_CS_PIN
-  #define X_CS_PIN         39   // PC7 X_nCS
+  #define X_CS_PIN         39
 #endif
 
-#define Y_STEP_PIN         51   // PC12 Y-STEP *
-#define Y_DIR_PIN          92   // PC11 Y-DIR -AddOns *
-#define Y_ENABLE_PIN       49   // PC14 Y-EN *
+#define Y_STEP_PIN         51
+#define Y_DIR_PIN          92
+#define Y_ENABLE_PIN       49
 #ifndef Y_CS_PIN
-  #define Y_CS_PIN         50   // PC13 Y_nCS
+  #define Y_CS_PIN         50
 #endif
 
-#define Z_STEP_PIN         46   // PC17 Z-STEP *
-#define Z_DIR_PIN          47   // PC16 Z-DIR *
-#define Z_ENABLE_PIN       44   // PC19 Z-END *
+#define Z_STEP_PIN         46
+#define Z_DIR_PIN          47
+#define Z_ENABLE_PIN       44
 #ifndef Z_CS_PIN
-  #define Z_CS_PIN         45   // PC18 Z_nCS
+  #define Z_CS_PIN         45
 #endif
 
-#define E0_STEP_PIN       107   // PB10 E1-STEP -AddOns *
-#define E0_DIR_PIN         96   // PC10 E1-DIR -AddOns *
-#define E0_ENABLE_PIN     105   // PB22 E1-EN -AddOns *
+#define E0_STEP_PIN       107
+#define E0_DIR_PIN         96
+#define E0_ENABLE_PIN     105
 #ifndef E0_CS_PIN
-  #define E0_CS_PIN       104   // PC20 E1_nCS -AddOns *
+  #define E0_CS_PIN       104
 #endif
 
-#define E1_STEP_PIN        22   // PB26 E2_STEP *
-#define E1_DIR_PIN         97   // PB24 E2_DIR -AddOns *
-#define E1_ENABLE_PIN      18   // PA11 E2-EN
+#define E1_STEP_PIN        22
+#define E1_DIR_PIN         97
+#define E1_ENABLE_PIN      18
 #ifndef E1_CS_PIN
-  #define E1_CS_PIN        19   // PA10 E2_nCS
+  #define E1_CS_PIN        19
 #endif
 
 //
@@ -132,36 +132,36 @@
 // Required for the Archim2 board.
 //
 #if ENABLED(TMC_USE_SW_SPI)
-  #define TMC_SW_MOSI      28   // PD3
-  #define TMC_SW_MISO      26   // PD1
-  #define TMC_SW_SCK       27   // PD2
+  #define TMC_SW_MOSI      28
+  #define TMC_SW_MISO      26
+  #define TMC_SW_SCK       27
 #endif
 
 //
 // Temperature Sensors
 //
-#define TEMP_0_PIN         10   // D10 PB19 THERM AN1 *
-#define TEMP_1_PIN          9   // D9 PB18 THERM AN2 *
-#define TEMP_2_PIN          8   // D8 PB17 THERM AN4 *
-#define TEMP_BED_PIN       11   // D11 PB20 THERM AN3 *
+#define TEMP_0_PIN         10
+#define TEMP_1_PIN          9
+#define TEMP_2_PIN          8
+#define TEMP_BED_PIN       11
 
 //
 // Heaters / Fans
 //
 #ifndef FAN_PIN
-  #define FAN_PIN           4   // D4 PC26 FET_PWM1
+  #define FAN_PIN           4
 #endif
-#define FAN1_PIN            5   // D5 PC25 FET_PWM2
+#define FAN1_PIN            5
 
-#define HEATER_0_PIN        6   // D6 PC24 FET_PWM3
-#define HEATER_1_PIN        7   // D7 PC23 FET_PWM4
-#define HEATER_2_PIN        8   // D8 PC22 FET_PWM5
-#define HEATER_BED_PIN      9   // D9 PC21 BED_PWM
+#define HEATER_0_PIN        6
+#define HEATER_1_PIN        7
+#define HEATER_2_PIN        8
+#define HEATER_BED_PIN      9
 
 //
 // Misc. Functions
 //
-#define SDSS               87   // D87 PA29 CS
+#define SDSS               87
 
 //////////////////////////
 // LCDs and Controllers //
@@ -169,19 +169,19 @@
 
 #if ENABLED(ULTRA_LCD)
   #if ENABLED(NEWPANEL)
-    #define BEEPER_PIN     23   // D24 PA15_CTS1
-    #define LCD_PINS_RS    17   // D17 PA12_RXD1
-    #define LCD_PINS_ENABLE 24  // D23 PA14_RTS1
-    #define LCD_PINS_D4    69   // D69 PA0_CANTX0
-    #define LCD_PINS_D5    54   // D54 PA16_SCK1
-    #define LCD_PINS_D6    68   // D68 PA1_CANRX0
-    #define LCD_PINS_D7    34   // D34 PC2_PWML0
+    #define BEEPER_PIN     23
+    #define LCD_PINS_RS    17
+    #define LCD_PINS_ENABLE 24
+    #define LCD_PINS_D4    69
+    #define LCD_PINS_D5    54
+    #define LCD_PINS_D6    68
+    #define LCD_PINS_D7    34
 
-    #define SD_DETECT_PIN   2   // D2  PB25_TIOA0
+    #define SD_DETECT_PIN   2
 
     // Buttons on AUX-2
-    #define BTN_EN1        60   // D60 PA3_TIOB1
-    #define BTN_EN2        13   // D13 PB27_TIOB0
-    #define BTN_ENC        16   // D16 PA13_TXD1 // the click
+    #define BTN_EN1        60
+    #define BTN_EN2        13
+    #define BTN_ENC        16
   #endif  // NEWPANEL
 #endif // ULTRA_LCD
